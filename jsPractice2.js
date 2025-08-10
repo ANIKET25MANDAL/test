e241e5e43c0f307613d96755cea4347542894ce0
ANIKET25MANDAL <aniketkumarmandal727@gmail.com>
2025-08-10 17:04:32 +0530

    golden string question was solved

--- jsPractice2.js
+++ jsPractice2.js
@@ -37,3 +37,16 @@ switch (quarter) {
         console.log("Enter the correct month");
 }
 
+/* Qs4. A string is a golden string if it starts with the character ‘A’ or ‘a’ and has a total length greater than 5.
+For a given string print if it is golden or not.  */
+let str = 'hNIKET';
+if ((str[0] == 'a' || 'A') && (str.length > 5)) {
+    console.log("golden string");
+}
+else {
+    console.log("not a golden string");
+}
+
+
+
+
