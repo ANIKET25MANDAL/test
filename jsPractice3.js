6ed58d75b6b1bcdb7e73f997d841db37401f0791
ANIKET25MANDAL <aniketkumarmandal727@gmail.com>
2025-08-12 15:23:49 +0530

    question 3 was solve

--- jsPractice3.js
+++ jsPractice3.js
@@ -17,3 +17,12 @@ let arr1 = [7, 9, 0, -2];
 arr1.shift();
 console.log(arr1);
 
+
+// Qs3. Write a JavaScript program to check whether a string is blank or not.
+let str = prompt("Enter a stirng");
+if (str.length == 0) {
+    console.log("string is empty");
+} else {
+    console.log("string is not empty");
+}
+
