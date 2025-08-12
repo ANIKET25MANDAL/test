b2bc0af30a00cfc864dbd86a3461b22dbeb93208
ANIKET25MANDAL <aniketkumarmandal727@gmail.com>
2025-08-12 15:16:04 +0530

    question 2 was solved

--- jsPractice3.js
+++ jsPractice3.js
@@ -6,3 +6,14 @@
 let arr = [7, 9, 0, -2];
 let n = 3;
 console.log(arr.slice(0, 3));
+
+
+/*    Qs2. Write a JavaScript program to get the last n elements of an array.[n can be any positive number].
+    Forexample: for array[7,9,0,-2] and n=3
+    Print,[9,0,-2
+*/
+
+let arr1 = [7, 9, 0, -2];
+arr1.shift();
+console.log(arr1);
+
