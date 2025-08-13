d9b349e2efe087d4ff0da515463e82c171f3a350
ANIKET25MANDAL <aniketkumarmandal727@gmail.com>
2025-08-13 21:03:39 +0530

    Practice 4 question number 4 was solved.

--- jsPractice4.js
+++ jsPractice4.js
@@ -36,3 +36,25 @@ while (n > 0) {
     sum = sum + last_digt;
 }
 console.log(sum);
+
+
+
+/*  Qs4. Print the factorial of a number n.
+    [Factorial of a number n is the product of all positive integers less than or equal to a given positive integer and denoted by that integer.]
+    Example:
+    7!(factorial of 7) = 1x2x3x4x5x6x7 = 5040
+    5!(factorial of 5) = 1x2x3x4x5 = 120
+    3!(factorial of 3) = 1x2x3 = 6 
+    0! Is always 1   */
+
+let f = prompt("Enter any number for which you want factorial");
+let factorial = 1;
+if (f == 0) {
+    console.log(`Factorial of ${f} is 1`);
+} else {
+    for (let i = f; i > 0; i--) {
+        factorial = factorial * i;
+    }
+}
+
+console.log(`Factorial of ${f} is ${factorial}`);
