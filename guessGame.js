075476a6856006003db257df9afad7c26f13f751
ANIKET25MANDAL <aniketkumarmandal727@gmail.com>
2025-08-14 15:25:49 +0530

    guess Game was created with the help of javaScript.

---
+++ guessGame.js
@@ -0,0 +1,22 @@
+const max = prompt("Enter any maximum number");
+
+
+const random = Math.floor(Math.random() * max) + 1;
+let guess = prompt("guess the number");
+
+while (true) {
+    if (guess == "quit") {
+        console.log("user quit");
+        break;
+    }
+    if (parseInt(guess) == random) {
+        console.log("your are right! congrats!", random);
+        break;
+    } else if (guess < random) {
+        guess = prompt("hint: your guess waas too small. please try agin");
+    } else if (guess > random) {
+        guess = prompt("hint: your guess was too big please try agian");
+    } else {
+        guess = prompt("your guess was wrong. Please try again");
+    }
+}
