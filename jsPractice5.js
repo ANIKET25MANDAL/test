a9a8880f608f16330c2ce17baf0bfb3d50311428
ANIKET25MANDAL <aniketkumarmandal727@gmail.com>
2025-08-14 15:40:52 +0530

    question 1 and question 2 of assignment 5 was solved

---
+++ jsPractice5.js
@@ -0,0 +1,16 @@
+/* Qs1. Create a program that generates a random number representing a dice roll.
+ [The number should be between 1 and 6].   */
+
+let dice = Math.floor(Math.random() * 6) + 1;
+console.log(dice);
+
+
+/* Qs2. Create an object representing a car that stores the following properties for the car: name, model, color.
+ Print the car’s name.  */
+
+let car = {
+    name: "xuv",
+    model: '2025',
+    color: 'yellow'
+};
+console.log(car);
