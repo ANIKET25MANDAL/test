a9bf619d742d1d28e01cbd1b709e444947ab0d89
ANIKET25MANDAL <aniketkumarmandal727@gmail.com>
2025-08-19 19:34:05 +0530

    Question number 2 of javaScript practice 8 was solved.

--- jsPractice8.js
+++ jsPractice8.js
@@ -2,11 +2,21 @@
 
 let arr = [12, 34, 56, 23, 45];
 
-const square= arr.map((e)=>{
-    return e*e;
+const square = arr.map((e) => {
+    return e * e;
 })
-let sum=square.reduce((a,e)=>{
-    return a+e;
+let sum = square.reduce((a, e) => {
+    return a + e;
 })
-let average=sum/square.length;
+let average = sum / square.length;
 console.log(average);
+
+
+
+// Qs2. Create a new array using the map function whose each element is equal to the original element plus 5.
+
+let a = [12, 34, 56, 78, -98];
+let an = a.map((e) => {
+    return e + 5;
+})
+console.log(an);
