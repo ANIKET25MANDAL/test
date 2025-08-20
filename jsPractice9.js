dcf10956f1c0f1a8830cd476efb1ce97d8c698ed
ANIKET25MANDAL <aniketkumarmandal727@gmail.com>
2025-08-20 19:02:32 +0530

    question number 5 of javascipt practice 9 was solved.

--- jsPractice9.js
+++ jsPractice9.js
@@ -26,9 +26,15 @@ button.style.color = 'white';
 Qs4. Create an h1 element on the page and set its text to “DOM Practice” underlined.
 Change its color to purple.
 */
-let h1=document.createElement('h1');
-h1.innerHTML="<u>DOM Practice</u>";
+let h1 = document.createElement('h1');
+h1.innerHTML = "<u>DOM Practice</u>";
 document.querySelector('body').append(h1);
-h1.style.color="purple";
-h1.style.textDecoration="underline";
+h1.style.color = "purple";
+h1.style.textDecoration = "underline";
 
+
+// Qs5. Create a p tag on the page and set its text to “Apna College Delta Practice”, where Delta is bold.
+
+let p = document.createElement('p');
+p.innerHTML = "Apna College <b> Delta </b> Practice";
+document.querySelector("body").append(p);
