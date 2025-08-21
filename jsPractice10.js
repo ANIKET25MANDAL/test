c674e18fd46b5707eba876db66c74157c73bf5c9
ANIKET25MANDAL <aniketkumarmandal727@gmail.com>
2025-08-21 20:02:45 +0530

    question 3 of javaScript practice 10 was solved.

--- jsPractice10.js
+++ jsPractice10.js
@@ -47,3 +47,22 @@ button.addEventListener("click",function(){
   button.style.color="green";
   button.style.backgroundColor="red";
 });
+
+
+
+/*
+Qs3. Create an input element on the page with a placeholder ”enter your name” and an H2 heading on the page inside HTML.
+The purpose of this input element is to enter a user’s name so it should only input letters from a-z, A-Z and space(all other characters should not be detected). 
+ Whenever the user inputs their name, their input should be dynamically visible inside the heading.
+ [Please note that no other character apart from the allowed characters should be visible in the heading]
+ */
+ let i=document.querySelector("#in");
+  let heading=document.querySelector("#display");
+ i.addEventListener("input",function(){
+    let cleanValue = this.value.replace(/[^a-zA-Z ]/g, "");
+    this.value = cleanValue; // Update the input field
+    heading.innerText = cleanValue; // Update the heading
+    console.log("key was pressed");
+ });
+
+ 
